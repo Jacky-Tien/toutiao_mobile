84cf0e4aa0f3299fc818ab12add00f42b7b1aaae
JackyTien <tianhuixiang168@qq.com>
2020-07-16 21:15:35 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant' // 导入Vant组件库
+import 'vant/lib/index.css' // Vant组件库样式
+
+import 'amfe-flexible'
+
+import '@/styles/index.less' // 导入全局样式表
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
